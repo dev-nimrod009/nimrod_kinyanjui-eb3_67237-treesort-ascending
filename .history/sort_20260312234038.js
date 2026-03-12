fb535b3e9eabe9943fb22d626808a52e17a7e0bc
dev-nimrod009 <nimrodkinyanjui003@gmail.com>
2026-03-12 23:50:08 +0300

    add insert method to BST with comparison and assignment counters

---
+++ .history/sort_20260312234038.js
@@ -0,0 +1,14 @@
+class TreeNode {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class TreeSort {
+    constructor() {
+        this.comparisons = 0;
+        this.assignments = 0;
+    }
+}
